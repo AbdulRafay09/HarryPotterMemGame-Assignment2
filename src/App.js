c18ec483da02b6c3270b31bfae854082a8d350a4
AbdulRafay09 <92826752+AbdulRafay09@users.noreply.github.com>
2023-03-27 10:58:01 -0400

    Added Boolean to check first choice and second choice onclick

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import './App.css';
 import SingleImage from './components/SingleImage';
 
 const hpImages = [
-  {"src": "/img/dumble.png", matched: false},
-  {"src": "/img/granger.png", matched: false},
-  {"src": "/img/harry.png", matched: false},
-  {"src": "/img/snape.png", matched: false},
-  {"src": "/img/sortinghat.png", matched: false},
-  {"src": "/img/wolde.png", matched: false}
+  {"src": "/img/dumble.png", matched: true},
+  {"src": "/img/granger.png", matched: true},
+  {"src": "/img/harry.png", matched: true},
+  {"src": "/img/snape.png", matched: true},
+  {"src": "/img/sortinghat.png", matched: true},
+  {"src": "/img/wolde.png", matched: true}
 ]
 
 function App() {
@@ -29,11 +29,11 @@ function App() {
   }
   useEffect(() => {
     if(firstChoice && secondChoice){
-      if(firstChoice.src === secondChoice.src){
+      if(firstChoice.src !== secondChoice.src){
         setImages(pastImages => {
           return pastImages.map(imageHP => {
-            if (imageHP.src === secondChoice.src){
-              return{...imageHP, matched: true}
+            if (imageHP.src !== secondChoice.src){
+              return{...imageHP, matched: false}
             }
             else{
               return imageHP
