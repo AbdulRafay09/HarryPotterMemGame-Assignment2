da89726479db8ccf63735a5a205a6fed59f4f2a7
AbdulRafay09 <92826752+AbdulRafay09@users.noreply.github.com>
2023-03-27 12:32:12 -0400

    Lost all my data so had to some stuff again

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import './App.css';
 import SingleImage from './components/SingleImage';
 
 const hpImages = [
-  {"src": "/img/dumble.png", matched: true},
-  {"src": "/img/granger.png", matched: true},
-  {"src": "/img/harry.png", matched: true},
-  {"src": "/img/snape.png", matched: true},
-  {"src": "/img/sortinghat.png", matched: true},
-  {"src": "/img/wolde.png", matched: true}
+  {"src": "/img/dumble.png", matched: false},
+  {"src": "/img/granger.png", matched: false},
+  {"src": "/img/harry.png", matched: false},
+  {"src": "/img/snape.png", matched: false},
+  {"src": "/img/sortinghat.png", matched: false},
+  {"src": "/img/wolde.png", matched: false}
 ]
 
 function App() {
@@ -29,11 +29,11 @@ function App() {
   }
   useEffect(() => {
     if(firstChoice && secondChoice){
-      if(firstChoice.src !== secondChoice.src){
+      if(firstChoice.src === secondChoice.src){
         setImages(pastImages => {
           return pastImages.map(imageHP => {
-            if (imageHP.src !== secondChoice.src){
-              return{...imageHP, matched: false}
+            if (imageHP.src === secondChoice.src){
+              return{...imageHP, matched: true}
             }
             else{
               return imageHP
@@ -61,7 +61,8 @@ function App() {
       
       <div className="image-grid">
         {imagesHP.map(imageHP => (
-          <SingleImage key={imageHP.id} imageHP={imageHP} choiceHandle={choiceHandle}/>
+          <SingleImage key={imageHP.id} imageHP={imageHP} choiceHandle={choiceHandle}
+          flipImage={imageHP === firstChoice || imageHP === secondChoice || imageHP.matched}/>
         ))}
       </div>
     </div>
--- src/components/SingleImage.js
+++ src/components/SingleImage.js
@@ -1,11 +1,11 @@
 import './SingleImage.css'
-export default function SingleImage({ imageHP, choiceHandle }){
+export default function SingleImage({ imageHP, choiceHandle, flipImage }){
     const clickHandle = () => {
         choiceHandle(imageHP)
     }
     return(
         <div className="imag"> 
-            <div>
+            <div className={flipImage ? "flipImage" : ""}>
                 <img className ="front" src={imageHP.src} alt="front"/> 
                 <img className ="back" src="/img/front.png" onClick={clickHandle} alt="back"/>
             </div>
